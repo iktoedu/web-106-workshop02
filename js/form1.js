e696e86dbaf9f12f6638205501a0aa51880a4355
Maxim Polishchuk <mpolishchuck@vu.cdu.edu.ua>
2018-10-22 23:25:46 +0300

    Add form1.js

---
+++ js/form1.js
@@ -0,0 +1,32 @@
+var submitActor = null;
+var numUses = 0;
+
+window.onload = function () {
+    document.getElementById('form1-add').onclick = function () {
+        submitActor = this;
+    };
+
+    document.getElementById('form1-subtract').onclick = function () {
+        submitActor = this;
+    };
+
+    document.getElementById('form1').onsubmit = function () {
+
+        var arg1 = document.getElementById('form1-arg1');
+        var arg2 = document.getElementById('form1-arg2');
+        var result;
+
+        if (submitActor.id == 'form1-add') {
+            result = parseInt(arg1.value) + parseInt(arg2.value);
+        }
+        else if (submitActor.id == 'form1-subtract') {
+            result = parseInt(arg1.value) - parseInt(arg2.value);
+        }
+
+        document.getElementById('form1-result').value = result;
+        document.getElementById('form1-num-uses').value = ++numUses;
+
+        return false;
+    };
+
+};
