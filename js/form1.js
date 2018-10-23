c3aed325a4812a2aebbe566a5c1955c62bc20768
Maxim Polishchuk <mpolishchuck@vu.cdu.edu.ua>
2018-10-23 08:35:45 +0300

    Put things toghther

--- js/form1.js
+++ js/form1.js
@@ -1,32 +1,35 @@
-var submitActor = null;
-var numUses = 0;
+(function () {
+    var submitActor = null;
+    var numUses = 0;
 
-window.onload = function () {
-    document.getElementById('form1-add').onclick = function () {
-        submitActor = this;
+    onLoadHandlers[onLoadHandlers.length] = function () {
+        document.getElementById('form1-add').onclick = function () {
+            submitActor = this;
+        };
+
+        document.getElementById('form1-subtract').onclick = function () {
+            submitActor = this;
+        };
+
+        document.getElementById('form1').onsubmit = function () {
+
+            var arg1 = document.getElementById('form1-arg1');
+            var arg2 = document.getElementById('form1-arg2');
+            var result;
+
+            if (submitActor.id == 'form1-add') {
+                result = parseInt(arg1.value) + parseInt(arg2.value);
+            }
+            else if (submitActor.id == 'form1-subtract') {
+                result = parseInt(arg1.value) - parseInt(arg2.value);
+            }
+
+            document.getElementById('form1-result').value = result;
+            document.getElementById('form1-num-uses').value = ++numUses;
+
+            return false;
+        };
+
     };
 
-    document.getElementById('form1-subtract').onclick = function () {
-        submitActor = this;
-    };
-
-    document.getElementById('form1').onsubmit = function () {
-
-        var arg1 = document.getElementById('form1-arg1');
-        var arg2 = document.getElementById('form1-arg2');
-        var result;
-
-        if (submitActor.id == 'form1-add') {
-            result = parseInt(arg1.value) + parseInt(arg2.value);
-        }
-        else if (submitActor.id == 'form1-subtract') {
-            result = parseInt(arg1.value) - parseInt(arg2.value);
-        }
-
-        document.getElementById('form1-result').value = result;
-        document.getElementById('form1-num-uses').value = ++numUses;
-
-        return false;
-    };
-
-};
+})();
--- js/form2.js
+++ js/form2.js
@@ -1,18 +1,21 @@
-var numUses = 0;
-var words = ['hello', 'guys', 'good', 'morning', 'these', 'words', 'are', 'not', 'random'];
-var wordsPosition = 0;
+(function () {
+    var numUses = 0;
+    var words = ['hello', 'guys', 'good', 'morning', 'these', 'words', 'are', 'not', 'random'];
+    var wordsPosition = 0;
 
-window.onload = function () {
-    document.getElementById('form2').onsubmit = function () {
+    onLoadHandlers[onLoadHandlers.length] = function () {
+        document.getElementById('form2').onsubmit = function () {
 
-        document.getElementById('form2-result').value = words[wordsPosition];
+            document.getElementById('form2-result').value = words[wordsPosition];
 
-        wordsPosition++;
-        if (wordsPosition >= words.length) {
-            wordsPosition = 0;
-        }
+            wordsPosition++;
+            if (wordsPosition >= words.length) {
+                wordsPosition = 0;
+            }
 
-        document.getElementById('form2-num-uses').value = ++numUses;
-        return false;
+            document.getElementById('form2-num-uses').value = ++numUses;
+            return false;
+        };
     };
-};
+
+})();
